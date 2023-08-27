e67d1540a7927dd4c0fea8b362857bb1f6fe91b7
kangqing <kangqing.37@gmail.com>
2023-08-27 17:57:01 +0800

    init

---
+++ src/main/java/com/kangqing/teamcity/TeamcityApplication.java
@@ -0,0 +1,13 @@
+package com.kangqing.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityApplication.class, args);
+    }
+
+}
